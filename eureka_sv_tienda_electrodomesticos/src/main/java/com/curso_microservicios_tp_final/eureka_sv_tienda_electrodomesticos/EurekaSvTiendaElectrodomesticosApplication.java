a8d5282b18c00a17f878d7cec6ae38a5f6b8b253
joaquinsch <joacohao1@gmail.com>
2026-05-29 00:39:26 -0300

    agrego eureka server

---
+++ eureka_sv_tienda_electrodomesticos/src/main/java/com/curso_microservicios_tp_final/eureka_sv_tienda_electrodomesticos/EurekaSvTiendaElectrodomesticosApplication.java
@@ -0,0 +1,13 @@
+package com.curso_microservicios_tp_final.eureka_sv_tienda_electrodomesticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvTiendaElectrodomesticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvTiendaElectrodomesticosApplication.class, args);
+	}
+
+}
