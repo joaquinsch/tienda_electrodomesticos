f0abe7f0bde958291d952c87b6f1adcab9abcd31
joaquinsch <joacohao1@gmail.com>
2026-05-29 01:11:44 -0300

    arreglo eureka-sv que no reconocia maven

---
+++ eureka-sv/src/main/java/com/curso_microservicios_tp_final/eureka_sv/EurekaSvApplication.java
@@ -0,0 +1,13 @@
+package com.curso_microservicios_tp_final.eureka_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvApplication.class, args);
+	}
+
+}
---
+++ eureka-sv/src/test/java/com/curso_microservicios_tp_final/eureka_sv/EurekaSvApplicationTests.java
@@ -0,0 +1,15 @@
+package com.curso_microservicios_tp_final.eureka_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
