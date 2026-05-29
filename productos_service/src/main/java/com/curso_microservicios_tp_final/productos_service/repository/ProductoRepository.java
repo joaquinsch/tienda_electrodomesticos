76b32b05d180b6d111b304967c3987b831ffe481
joaquinsch <joacohao1@gmail.com>
2026-05-29 00:29:30 -0300

    creo servicio de productos

---
+++ productos_service/src/main/java/com/curso_microservicios_tp_final/productos_service/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.curso_microservicios_tp_final.productos_service.repository;
+
+import com.curso_microservicios_tp_final.productos_service.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
---
+++ productos_service/src/test/java/com/curso_microservicios_tp_final/productos_service/ProductosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_microservicios_tp_final.productos_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
