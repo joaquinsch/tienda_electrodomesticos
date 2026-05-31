af9ab0158acb53e2cb97830130c40cdc9f03559a
joaquinsch <joacohao1@gmail.com>
2026-05-30 21:19:51 -0300

    el carrito se crea pasando y verificando los codigo_producto de cada producto

--- carrito_compras/src/main/java/com/curso_microservicios_tp_final/carrito_compras/service/CarritoService.java
+++ carrito_compras/src/main/java/com/curso_microservicios_tp_final/carrito_compras/service/CarritoService.java
@@ -6,6 +6,9 @@ import com.curso_microservicios_tp_final.carrito_compras.repository.CarritoRepos
 import com.curso_microservicios_tp_final.carrito_compras.repository.ProductosAPIClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CarritoService implements ICarritoService {
 
@@ -19,14 +22,19 @@ public class CarritoService implements ICarritoService {
 
     @Override
     public Carrito crearCarrito(Carrito carrito) {
-        verificarProductos(carrito);
+        List<ProductoDTO> productosVerificados = obtenerProductos(carrito);
+        carrito.setLista_productos(productosVerificados);
         return carritoRepository.save(carrito);
     }
 
-    private void verificarProductos(Carrito carrito) {
+    private List<ProductoDTO> obtenerProductos(Carrito carrito) {
+        List<ProductoDTO> productosRecuperados = new ArrayList<>();
         for (ProductoDTO prod : carrito.getLista_productos()){
+            // si no lo encuentra tira una excepcion
             ProductoDTO actual = productosAPIClient.obtenerProducto(prod.getCodigo_producto());
+            productosRecuperados.add(actual);
         }
+        return productosRecuperados;
     }
 
 }
