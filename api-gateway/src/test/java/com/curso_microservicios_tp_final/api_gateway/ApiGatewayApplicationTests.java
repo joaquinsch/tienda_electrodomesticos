d1eb802e1f7a246157e6d872cef43b27460c1439
joaquinsch <joacohao1@gmail.com>
2026-06-02 00:54:27 -0300

    agrego api-gateway al proyecto

---
+++ api-gateway/src/test/java/com/curso_microservicios_tp_final/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_microservicios_tp_final.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
